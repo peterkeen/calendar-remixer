28dc6ece62d22cfb8f619cecb0f6ca800071a360
Pete Keen <pete@petekeen.net>
2019-10-06 20:33:43 -0400

    load not parse

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require './remixer'
 require 'sinatra'
 
 if ENV['CALENDARS']
-  calendars = YAML.parse(ENV.fetch('CALENDARS'))
+  calendars = YAML.load(ENV.fetch('CALENDARS'))
 else
   calendars = YAML.load_file(ENV.fetch('CALENDARS_PATH', 'calendars.yaml'))
 end
